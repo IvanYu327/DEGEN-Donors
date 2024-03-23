bd7f0e9c790fd59354fd3785373d70a4252a75c1
Ivan <ivanyu327@gmail.com>
2024-03-23 17:03:12 -0400

    yes

--- framesjs-starter/app/page.tsx
+++ framesjs-starter/app/page.tsx
@@ -15,18 +15,28 @@ import { currentURL } from "./utils";
 import Image from "next/image";
 
 type State = {
-  active: string;
-  total_button_presses: number;
+  title: string;
+  description: string;
+  raised: number;
+  goal: number;
+  days_left: number;
 };
 
-const initialState = { active: "1", total_button_presses: 0 };
+const initialState = {
+  title: "Ugandan Goats",
+  description: "Every $50 buys a goat for a family in Uganda.",
+  raised: 345,
+  goal: 1000,
+  days_left: 26,
+};
 
 const reducer: FrameReducer<State> = (state, action) => {
   return {
-    total_button_presses: state.total_button_presses + 1,
-    active: action.postBody?.untrustedData.buttonIndex
-      ? String(action.postBody?.untrustedData.buttonIndex)
-      : "1",
+    ...state, // Spread the existing state
+    // total_button_presses: state.total_button_presses + 1,
+    // active: action.postBody?.untrustedData.buttonIndex
+    //   ? String(action.postBody?.untrustedData.buttonIndex)
+    //   : "1",
   };
 };
 
@@ -54,6 +64,9 @@ export default async function Home({ searchParams }: NextServerPageProps) {
 
   console.log("info: state is:", state);
 
+  const percent = (state.raised / state.goal) * 100;
+  const percent_string = percent.toFixed(2) + "%";
+
   // then, when done, return next frame
   return (
     <div className="p-4">
@@ -79,35 +92,43 @@ export default async function Home({ searchParams }: NextServerPageProps) {
               <div>Donate Now</div>
             </div>
             {/* Image, with title, and description to the right */}
-            <div tw="flex flex-row">
+            <div tw="flex flex-col w-95/100">
               <div tw="flex flex-row">
-                {/* <Image
+                <div tw="flex flex-row">
+                  {/* <Image
                   src="/your-image.jpg"
                   alt="Your Image"
                   width={500}
                   height={500}
                 /> */}
-                <div tw="flex bg-gray-300 w-64 h-64"></div>
+                  <div tw="flex bg-gray-300 w-64 h-64"></div>
+                </div>
+                <div tw="flex flex-col">
+                  <h2>{state?.title}</h2>
+                  <p>{state?.description}</p>
+                </div>
               </div>
-              <div tw="flex flex-col">
-                <h2>Title</h2>
-                <p>Description</p>
-              </div>
-            </div>
-            {/* Progress bar */}
-            <div tw="flex flex-col items-center">
-              <div tw="flex w-80/100 h-8 bg-red-500 rounded-lg overflow-hidden">
+              {/* Progress bar */}
+              <div tw="flex flex-col items-center">
                 <div
-                  tw="flex bg-blue-500 h-full"
-                  style={{ width: "70%" }}
-                ></div>
-                {/* Adjust width dynamically */}
+                  style={{ background: "#D9D9D9" }}
+                  tw="flex w-95/100 h-8 rounded-lg overflow-hidden"
+                >
+                  <div
+                    tw="h-full"
+                    style={{ background: "#285FCD", width: percent_string }}
+                  ></div>
+                  {/* Adjust width dynamically */}
+                </div>
+                {/* Progress bar info */}
+                <div tw="flex w-95/100 h-16 justify-between">
+                  <div tw="flex">
+                    <span tw="pr-2">${state?.raised}</span>
+                    of ${state?.goal} raised
+                  </div>
+                  <div tw="flex">{state?.days_left} days left</div>
+                </div>
               </div>
-            </div>
-            {/* $500 of $1000 raised left justified and 26 days left, right justified */}
-            <div tw="flex justify-between px-12">
-              <div>$500 of $1000 raised</div>
-              <div>26 days left</div>
             </div>
           </div>
         </FrameImage>
